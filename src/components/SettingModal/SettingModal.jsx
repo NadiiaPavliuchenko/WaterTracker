b01f30219559d85b7da8050c4b6acec47587b5e2
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-03-29 15:35:57 +0200

    start settings

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,7 +1,132 @@
-import { StyledDiv } from './SettingModal.styled';
+import {
+  ModalDiv,
+  VisuallyHiddenInput,
+  TitleWrapper,
+  CloseBtn,
+  FormSubtitle,
+  FormLabel,
+  SmallLabel,
+  ImgThumb,
+  FilePickerWrapper,
+  FilePickerLink,
+  RadioGroup,
+} from './SettingModal.styled';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { Formik, Form, Field } from 'formik';
 
 const SettingModal = () => {
-  return <StyledDiv>SettingModal</StyledDiv>;
+  return (
+    <ModalDiv>
+      <TitleWrapper>
+        <h3>Settings</h3>
+        <CloseBtn>
+          <CloseRoundedIcon />
+        </CloseBtn>
+      </TitleWrapper>
+      <Formik
+        initialValues={{
+          gender: '',
+          name: '',
+          email: '',
+          outdatedPassword: '',
+          newPassword: '',
+          repetedPassword: '',
+        }}
+        onSubmit={({ setSubmitting }) => {
+          console.log('Form is validated! Submitting the form...');
+          setSubmitting(false);
+        }}
+      >
+        {() => (
+          <Form>
+            <div>
+              <FormLabel htmlFor="file">Your photo</FormLabel>
+              <FilePickerWrapper>
+                <ImgThumb>
+                  <img src="" alt="" />
+                </ImgThumb>
+                <FilePickerLink
+                  component="label"
+                  underline="none"
+                  accept=".jpg,.jpeg,.png"
+                  tabIndex={-1}
+                >
+                  <FileUploadOutlinedIcon />
+                  Upload a photo
+                  <VisuallyHiddenInput type="file" name="file" />
+                </FilePickerLink>
+              </FilePickerWrapper>
+            </div>
+            <FormSubtitle id="my-radio-group">
+              Your gender identity
+            </FormSubtitle>
+            <RadioGroup role="group" aria-labelledby="my-radio-group">
+              <SmallLabel>
+                <Field type="radio" name="picked" value="woman" />
+                Woman
+              </SmallLabel>
+              <SmallLabel>
+                <Field type="radio" name="picked" value="man" />
+                Man
+              </SmallLabel>
+            </RadioGroup>
+            <div className="form-group">
+              <FormLabel htmlFor="name">Your name</FormLabel>
+              <Field type="name" name="name" className="form-control" />
+            </div>
+            <div className="form-group">
+              <FormLabel htmlFor="email">E-mail</FormLabel>
+              <Field
+                type="email"
+                name="email"
+                className="form-control"
+                autoComplete="email"
+              />
+            </div>
+            <div>
+              <FormSubtitle>Password</FormSubtitle>
+              <div className="form-group">
+                <SmallLabel htmlFor="outdatedPassword">
+                  Outdated Password:
+                </SmallLabel>
+                <Field
+                  type="password"
+                  name="outdatedPassword"
+                  className="form-control"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+              </div>
+              <div className="form-group">
+                <SmallLabel htmlFor="newPassword">New Password:</SmallLabel>
+                <Field
+                  type="password"
+                  name="newPassword"
+                  className="form-control"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+              </div>
+              <div className="form-group">
+                <SmallLabel htmlFor="repetedPassword">
+                  Repeat new Password:
+                </SmallLabel>
+                <Field
+                  type="password"
+                  name="repetedPassword"
+                  className="form-control"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+              </div>
+            </div>
+            <button type="submit">Save</button>
+          </Form>
+        )}
+      </Formik>
+    </ModalDiv>
+  );
 };
 
 export default SettingModal;
--- src/components/SettingModal/SettingModal.styled.jsx
+++ src/components/SettingModal/SettingModal.styled.jsx
@@ -1,6 +1,104 @@
 import styled from 'styled-components';
+import { Link } from '@mui/material';
 
-export const StyledDiv = styled.div`
+const size = {
+  mobile: '320px',
+  tablet: '768px',
+  desktop: '1440px',
+};
+
+const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
+
+export const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+export const ModalDiv = styled.div`
+  border-radius: 10px;
+  border: 1px solid black;
+  padding: 32px 12px;
+  @media ${device.tablet} {
+    padding: 32px 24px;
+  }
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
+export const Title = styled.h2`
+  font-size: 26px;
   font-family: var(--roboto-bold);
+`;
+
+export const CloseBtn = styled.button`
+  border: none;
+  background-color: transparent;
   color: var(--primary-blue);
+  width: 24px;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FormSubtitle = styled.h3`
+  font-size: 18px;
+  font-family: var(--roboto-bold);
+  margin-bottom: 12px;
+`;
+
+export const FormLabel = styled.label`
+  font-size: 18px;
+  font-family: var(--roboto-bold);
+`;
+
+export const SmallLabel = styled.label`
+  font-size: 16px;
+  font-family: var(--roboto-regular);
+`;
+
+export const FilePickerWrapper = styled.div`
+  margin: 8px 0 24px 0;
+  display: flex;
+  gap: 8px;
+`;
+
+export const ImgThumb = styled.div`
+  border-radius: 50%;
+  overflow: hidden;
+  width: 80px;
+  height: 80px;
+`;
+
+export const FilePickerLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  color: var(--primary-blue);
+  font-family: var(--roboto-medium);
+  font-size: 14px;
+`;
+
+export const RadioGroup = styled.div`
+  margin-bottom: 24px;
+  @media ${device.desktop} {
+    margin-bottom: 52px;
+  }
 `;
