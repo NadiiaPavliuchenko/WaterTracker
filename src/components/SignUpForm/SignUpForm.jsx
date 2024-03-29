ba7ba4e64b45f51c858531a83a6295ddf6189adb
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2024-03-29 21:15:48 +0200

    01

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,38 @@
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import * as yup from "yup"
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  let registerSchema = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().min(6).max(64).required()
+  })
+  const initialValues = { 
+    email: '',
+    password: ''
+  }
+  const handleSubmit = () => { 
+    console.log(values)
+  }  
+
+  return <>
+    <Formik initialValues={ initialValues} validationSchema={registerSchema} onSubmit={handleSubmit}>
+      <Form>
+        <label>Enter your email
+          <input type="email" name="email" />
+          <ErrorMessage name="email"/>
+        </label>
+        <label>Enter your password
+          <input type="password" name="password" />
+          <ErrorMessage name="password"/>
+        </label>
+        <label>Repeat password
+          <input type="password" name="repeatPassword" />          
+        </label>
+        <button type="submit">Sign Up</button>
+
+      </Form>
+
+  </Formik>
+    </>  
 };
 
 export default SignUpForm;
