0bc75696b4cc93c3b1ba97e8123fea2b1bd7d8bc
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-03-30 23:21:14 +0200

    copy

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -18,9 +18,8 @@ import {
   CustomRadio,
   SubmitButton,
   VisibilityIconsWrapper,
-  ModalContentWrapper,
 } from './SettingModal.styled';
-import { getCurrentUser } from '../../store/auth/authSelectors';
+// import { getCurrentUser } from '../../store/auth/authSelectors';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
@@ -28,14 +27,20 @@ import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined
 import { Button } from '@mui/material';
 import Modal from '@mui/material/Modal';
 
-import { Formik, Form } from 'formik';
+import { useFormik, Form, Formik } from 'formik';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 const SettingModal = () => {
   // const dispatch = useDispatch();
-  const user = useSelector(getCurrentUser);
-  console.log(user);
+  // const user = useSelector(getCurrentUser);
+  const user = {
+    name: 'n.pavljuchenko',
+    email: 'n.pavljuchenko@gmail.com',
+    avatarURL: '//www.gravatar.com/avatar/397507de0f385b925fb12604e5e147b1',
+    norm: 2000,
+    gender: 'woman',
+  };
 
   const [showPassword, setShowPassword] = useState([false, false, false]);
   const [open, setOpen] = useState(false);
@@ -46,6 +51,24 @@ const SettingModal = () => {
     newShowPassword[index] = !showPassword[index];
     setShowPassword(newShowPassword);
   };
+
+  const formik = useFormik({
+    initialValues: {
+      gender: user.gender,
+      name: user.name,
+      email: user.email,
+      avatarURL: user.avatarURL,
+      outdatedPassword: '',
+      newPassword: '',
+      repetedPassword: '',
+    },
+    onSubmit: (values) => {
+      console.log(
+        'Form is validated! Submitting the form...',
+        JSON.stringify(values, null, 2)
+      );
+    },
+  });
 
   return (
     <div>
@@ -63,147 +86,130 @@ const SettingModal = () => {
               <CloseRoundedIcon />
             </CloseBtn>
           </TitleWrapper>
-          <Formik
-            initialValues={{
-              gender: '',
-              name: '',
-              email: '',
-              outdatedPassword: '',
-              newPassword: '',
-              repetedPassword: '',
-            }}
-            onSubmit={({ setSubmitting }) => {
-              console.log('Form is validated! Submitting the form...');
-              setSubmitting(false);
-            }}
-          >
-            {() => (
-              <Form>
-                <ModalContentWrapper>
-                  <div>
-                    <FormLabel htmlFor="file">Your photo</FormLabel>
-                    <FilePickerWrapper>
-                      <ImgThumb>
-                        <img src="" alt="" />
-                      </ImgThumb>
-                      <FilePickerLink
-                        component="label"
-                        underline="none"
-                        accept=".jpg,.jpeg,.png"
-                        tabIndex={-1}
-                      >
-                        <FileUploadOutlinedIcon />
-                        Upload a photo
-                        <VisuallyHiddenInput type="file" name="file" />
-                      </FilePickerLink>
-                    </FilePickerWrapper>
-                  </div>
-                  <FormContentWrapper>
-                    <div>
-                      <FormSubtitle id="my-radio-group">
-                        Your gender identity
-                      </FormSubtitle>
-                      <StyledRadioGroup row aria-labelledby="my-radio-group">
-                        <SmallControlLabel
-                          value="female"
-                          control={<CustomRadio disableTouchRipple />}
-                          label="Woman"
-                        ></SmallControlLabel>
-                        <SmallControlLabel
-                          value="male"
-                          control={<CustomRadio disableTouchRipple />}
-                          label="Man"
-                        ></SmallControlLabel>
-                      </StyledRadioGroup>
-                      <FormGroup>
-                        <FormLabel htmlFor="name">Your name</FormLabel>
-                        <StyledField
-                          type="name"
-                          name="name"
-                          className="form-control"
-                        />
-                      </FormGroup>
-                      <FormGroup>
-                        <FormLabel htmlFor="email">E-mail</FormLabel>
-                        <StyledField
-                          type="email"
-                          name="email"
-                          className="form-control"
-                          autoComplete="email"
-                        />
-                      </FormGroup>
-                    </div>
-                    <div>
-                      <FormSubtitle>Password</FormSubtitle>
-                      <PasswordFormGroup>
-                        <SmallLabel htmlFor="outdatedPassword">
-                          Outdated Password:
-                        </SmallLabel>
-                        <StyledField
-                          type={showPassword[0] ? 'text' : 'password'}
-                          name="outdatedPassword"
-                          className="form-control"
-                          placeholder="Password"
-                          autoComplete="current-password"
-                        />
-                        <VisibilityIconsWrapper
-                          onClick={() => handleShowPassword(0)}
-                        >
-                          {showPassword[0] ? (
-                            <VisibilityOutlinedIcon />
-                          ) : (
-                            <VisibilityOffOutlinedIcon />
-                          )}
-                        </VisibilityIconsWrapper>
-                      </PasswordFormGroup>
-                      <PasswordFormGroup>
-                        <SmallLabel htmlFor="newPassword">
-                          New Password:
-                        </SmallLabel>
-                        <StyledField
-                          type={showPassword[1] ? 'text' : 'password'}
-                          name="newPassword"
-                          className="form-control"
-                          placeholder="Password"
-                          autoComplete="current-password"
-                        />
-                        <VisibilityIconsWrapper
-                          onClick={() => handleShowPassword(1)}
-                        >
-                          {showPassword[1] ? (
-                            <VisibilityOutlinedIcon />
-                          ) : (
-                            <VisibilityOffOutlinedIcon />
-                          )}
-                        </VisibilityIconsWrapper>
-                      </PasswordFormGroup>
-                      <PasswordFormGroup>
-                        <SmallLabel htmlFor="repetedPassword">
-                          Repeat new Password:
-                        </SmallLabel>
-                        <StyledField
-                          type={showPassword[2] ? 'text' : 'password'}
-                          name="repetedPassword"
-                          className="form-control"
-                          placeholder="Password"
-                          autoComplete="current-password"
-                        />
-                        <VisibilityIconsWrapper
-                          onClick={() => handleShowPassword(2)}
-                        >
-                          {showPassword[2] ? (
-                            <VisibilityOutlinedIcon />
-                          ) : (
-                            <VisibilityOffOutlinedIcon />
-                          )}
-                        </VisibilityIconsWrapper>
-                      </PasswordFormGroup>
-                    </div>
-                  </FormContentWrapper>
-                </ModalContentWrapper>
-                <SubmitButton type="submit">Save</SubmitButton>
-              </Form>
-            )}
+          <Formik>
+            <Form>
+              <div>
+                <FormLabel htmlFor="file">Your photo</FormLabel>
+                <FilePickerWrapper>
+                  <ImgThumb>
+                    <img src={user.avatarURL} alt="" />
+                  </ImgThumb>
+                  <FilePickerLink
+                    component="label"
+                    underline="none"
+                    accept=".jpg,.jpeg,.png"
+                    tabIndex={-1}
+                  >
+                    <FileUploadOutlinedIcon />
+                    Upload a photo
+                    <VisuallyHiddenInput type="file" name="file" />
+                  </FilePickerLink>
+                </FilePickerWrapper>
+              </div>
+              <FormContentWrapper>
+                <div>
+                  <FormSubtitle id="my-radio-group">
+                    Your gender identity
+                  </FormSubtitle>
+                  <StyledRadioGroup row aria-labelledby="my-radio-group">
+                    <SmallControlLabel
+                      value="female"
+                      control={<CustomRadio disableTouchRipple />}
+                      label="Woman"
+                    ></SmallControlLabel>
+                    <SmallControlLabel
+                      value="male"
+                      control={<CustomRadio disableTouchRipple />}
+                      label="Man"
+                    ></SmallControlLabel>
+                  </StyledRadioGroup>
+                  <FormGroup>
+                    <FormLabel htmlFor="name">Your name</FormLabel>
+                    <StyledField
+                      type="name"
+                      name="name"
+                      className="form-control"
+                      onChange={formik.handleChange}
+                      value={formik.values.name}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <FormLabel htmlFor="email">E-mail</FormLabel>
+                    <StyledField
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      autoComplete="email"
+                    />
+                  </FormGroup>
+                </div>
+                <div>
+                  <FormSubtitle>Password</FormSubtitle>
+                  <PasswordFormGroup>
+                    <SmallLabel htmlFor="outdatedPassword">
+                      Outdated Password:
+                    </SmallLabel>
+                    <StyledField
+                      type={showPassword[0] ? 'text' : 'password'}
+                      name="outdatedPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      autoComplete="current-password"
+                    />
+                    <VisibilityIconsWrapper
+                      onClick={() => handleShowPassword(0)}
+                    >
+                      {showPassword[0] ? (
+                        <VisibilityOutlinedIcon />
+                      ) : (
+                        <VisibilityOffOutlinedIcon />
+                      )}
+                    </VisibilityIconsWrapper>
+                  </PasswordFormGroup>
+                  <PasswordFormGroup>
+                    <SmallLabel htmlFor="newPassword">New Password:</SmallLabel>
+                    <StyledField
+                      type={showPassword[1] ? 'text' : 'password'}
+                      name="newPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      autoComplete="current-password"
+                    />
+                    <VisibilityIconsWrapper
+                      onClick={() => handleShowPassword(1)}
+                    >
+                      {showPassword[1] ? (
+                        <VisibilityOutlinedIcon />
+                      ) : (
+                        <VisibilityOffOutlinedIcon />
+                      )}
+                    </VisibilityIconsWrapper>
+                  </PasswordFormGroup>
+                  <PasswordFormGroup>
+                    <SmallLabel htmlFor="repetedPassword">
+                      Repeat new Password:
+                    </SmallLabel>
+                    <StyledField
+                      type={showPassword[2] ? 'text' : 'password'}
+                      name="repetedPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      autoComplete="current-password"
+                    />
+                    <VisibilityIconsWrapper
+                      onClick={() => handleShowPassword(2)}
+                    >
+                      {showPassword[2] ? (
+                        <VisibilityOutlinedIcon />
+                      ) : (
+                        <VisibilityOffOutlinedIcon />
+                      )}
+                    </VisibilityIconsWrapper>
+                  </PasswordFormGroup>
+                </div>
+              </FormContentWrapper>
+              <SubmitButton type="submit">Save</SubmitButton>
+            </Form>
           </Formik>
         </ModalBox>
       </Modal>
