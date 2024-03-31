2782cc5c599d5d90c8e598ec13194b36092357de
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-04-01 01:23:36 +0300

    change deploy

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/WaterTrackerFrontend">
+        <BrowserRouter basename="/it-frogs-tp-03-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
