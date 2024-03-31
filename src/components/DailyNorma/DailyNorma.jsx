32779d89c7af196d022a189860978a7ed73a516b
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2024-03-31 18:06:54 +0200

    daily norma comp

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,12 +1,33 @@
 import DailyNormaModal from 'components/DailyNormaModal/DailyNormaModal';
+import { useSelector } from 'react-redux';
+import { getCurrentUser } from '../../store/auth/authSelectors';
+import { useState } from 'react';
 
-const DailyNorma = () => {
+export const DailyNorma = () => {
+  const { waterRate } = useSelector(getCurrentUser);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  // Перетворення мл у л
+  const waterRateL = (waterRate / 1000).toFixed(1) + ' L';
+
   return (
-    <>
-      <div>DailyNorma</div>
-      <DailyNormaModal />
-    </>
+    <div>
+      <div>
+        <h2>My daily norma: </h2>
+        <div>
+          <p>{waterRateL}</p>
+          <button onClick={openModal}>Edit</button>
+        </div>
+        <DailyNormaModal onClose={closeModal} onShow={isModalOpen} />
+      </div>
+    </div>
   );
 };
-
-export default DailyNorma;
