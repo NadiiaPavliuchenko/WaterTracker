f14f9a16d33247f79f7c04afd925776291344b3e
bogdansolomich <bogdansolomich@gmail.com>
2024-03-31 16:38:19 +0300

    обнова хедера

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,54 @@
-import Logo from 'components/Logo/Logo';
-import UserAuth from 'components/UserAuth/UserAuth';
-import UserLogo from 'components/UserLogo/UserLogo';
-import Loader from '../Loader/Loader';
+// import { Logo, Auth } from 'components/Logo/Logo';
+// import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+// import { getToken, getIsDarkTheme } from '../../redux/auth/auth-Selectors';
+// import { useSelector } from 'react-redux';
+import { ContainerHeader, CloseIcon } from './Header.styled';
+import sprite from 'src/assets/sprite.svg';
+import Wrapper from 'components/Wrapper/Wrapper';
+// import UserLogo from 'components/UserLogo/UserLogo';
+// import ThemeSwitcher from 'components/ThemeSwitcher/ThemeSwitcher';
+import UserLogoutModal from 'components/UserLogoutModal/UserLogoutModal';
+// import { selectIsLoggedIn } from 'components/UserAuth/authSelectors';
 
 export const Header = () => {
+  // const token = useSelector(getToken); // если авторизирован то токен есть
+  // const isDark = useSelector(getIsDarkTheme);
+
+  const token = true;
+  const isDark = false;
+
   return (
-    <>
-      <div>Header</div>
-      <Logo />
-      <UserLogo />
-      <UserAuth />
-      <Loader />
-    </>
+    // <WrapHeader $isDark={isDark}>
+    <Wrapper>
+      <ContainerHeader>
+        <div>
+          <NavLink to={token ? '/main' : '/'} className="logoBox">
+            <div>
+              <img src="/src/assets/LogoSite.png" alt="iconHome" />
+            </div>
+            <div className="logoText">TRACKER of water</div>
+          </NavLink>
+          {/* <ToggleThemeBtn /> */}
+          {!token && (
+            <div>
+              <NavLink className="textSignIn" to="login">
+                Sign in
+                <CloseIcon>
+                  <use href={`${sprite}#user`}></use>
+                </CloseIcon>
+              </NavLink>
+              <div>
+                <img src={isDark} alt="iconHome" />
+              </div>
+            </div>
+          )}
+
+          {token && <UserLogoutModal />}
+        </div>
+      </ContainerHeader>
+    </Wrapper>
   );
 };
+
+export default Header;
---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const ContainerHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 12px;
+`;
+
+export const WrapHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  flex-direction: column;
+`;
+
+export const CloseIcon = styled.svg`
+  margin-left: 8px;
+  width: 28px;
+  height: 28px;
+  stroke: rgb(47, 47, 47);
+  fill: transparent;
+`;
