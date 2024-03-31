65bce0fb1f9b642a50995b4b3836245aec5b06f0
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-03-31 20:21:21 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/WaterTrackerFrontend@08ec6acc7c8fe65a2d9869a2b25cebde804c4295 🚀

---
+++ assets/SignupPage-07279999.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-f67a5d59.js";const n=()=>r.jsx("div",{children:"SignUpForm"}),i=()=>r.jsxs(r.Fragment,{children:[r.jsx("div",{children:"SignupPage"}),r.jsx(n,{})]});export{i as default};
--- assets/SignupPage-b2009ef4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ac56bcf0.js";const n=()=>r.jsx("div",{children:"SignUpForm"}),i=()=>r.jsxs(r.Fragment,{children:[r.jsx("div",{children:"SignupPage"}),r.jsx(n,{})]});export{i as default};
