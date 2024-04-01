b1b41e71d408fd86cc55612924f8230c05a3a44f
roLLlyTHuK <ircvipua@gmail.com>
2024-04-01 14:45:27 +0300

    add hooks

--- src/components/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -1,38 +1,28 @@
-import { useState } from 'react';
 import { ButtonContainer, ModalBox } from './UserLogoutModal.styled';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import ModalContainer from '../ModalContainer/ModalContainer';
 import { useDispatch } from 'react-redux';
 import { logOutAPI } from '../../store/auth/authOperations';
+import useModal from '../../customHooks/useModal';
 
 const UserLogoutModal = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  const dispatch = useDispatch();
-
-  const handleClose = () => {
-    setIsOpen(false);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'Escape') {
-      setIsOpen(false);
-    }
-  };
+  const { isOpen, closeModal, handleKeyDown } = useModal();
 
   document.addEventListener('keydown', handleKeyDown);
 
+  const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(logOutAPI());
   };
   return (
     <>
       {isOpen && (
-        <ModalContainer onClose={handleClose}>
+        <ModalContainer onClose={closeModal}>
           <ModalBox>
             <div>
               <div className="topline">
                 <h2>Log out</h2>
-                <CloseOutlinedIcon className="close" onClick={handleClose} />
+                <CloseOutlinedIcon className="close" onClick={closeModal} />
               </div>
               <p>Do you really want to leave?</p>
             </div>
@@ -40,7 +30,7 @@ const UserLogoutModal = () => {
               <button className="confirm" onClick={handleLogout}>
                 Log out
               </button>
-              <button className="cancel" onClick={handleClose}>
+              <button className="cancel" onClick={closeModal}>
                 Cancel
               </button>
             </ButtonContainer>
