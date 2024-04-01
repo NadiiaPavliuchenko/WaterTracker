a5b7a3dec11094999b2d544d5d15fb37ff5e3e4b
roLLlyTHuK <ircvipua@gmail.com>
2024-04-01 13:13:43 +0300

    add function

--- src/components/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -1,25 +1,53 @@
-import ModalContainer from '../ModalContainer/ModalContainer';
+import { useState } from 'react';
 import { ButtonContainer, ModalBox } from './UserLogoutModal.styled';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import ModalContainer from '../ModalContainer/ModalContainer';
+import { useDispatch } from 'react-redux';
+import { logOutAPI } from '../../store/auth/authOperations';
 
 const UserLogoutModal = () => {
-  return (
-    <ModalContainer>
-      <ModalBox>
-        <div>
-          <div className="topline">
-            <h2>Log out</h2>
-            <CloseOutlinedIcon className="close" />
-          </div>
+  const [isOpen, setIsOpen] = useState(true);
+  const dispatch = useDispatch();
 
-          <p>Do you really want to leave?</p>
-        </div>
-        <ButtonContainer>
-          <button className="confirm">Log out</button>
-          <button className="cancel">Cancel</button>
-        </ButtonContainer>
-      </ModalBox>
-    </ModalContainer>
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Escape') {
+      setIsOpen(false);
+    }
+  };
+
+  document.addEventListener('keydown', handleKeyDown);
+
+  const handleLogout = () => {
+    dispatch(logOutAPI());
+  };
+  return (
+    <>
+      {isOpen && (
+        <ModalContainer onClose={handleClose}>
+          <ModalBox>
+            <div>
+              <div className="topline">
+                <h2>Log out</h2>
+                <CloseOutlinedIcon className="close" onClick={handleClose} />
+              </div>
+              <p>Do you really want to leave?</p>
+            </div>
+            <ButtonContainer>
+              <button className="confirm" onClick={handleLogout}>
+                Log out
+              </button>
+              <button className="cancel" onClick={handleClose}>
+                Cancel
+              </button>
+            </ButtonContainer>
+          </ModalBox>
+        </ModalContainer>
+      )}
+    </>
   );
 };
 
