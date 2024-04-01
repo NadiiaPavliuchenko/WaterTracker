bff44c1b260ec76cbb5e74e5ab0b281369cec7d9
roLLlyTHuK <ircvipua@gmail.com>
2024-04-01 15:39:38 +0300

    add ani waves, bottle, splash 1-2, shadow

---
+++ src/components/Animation/WavesAnimation.jsx
@@ -0,0 +1,28 @@
+import { Svg } from './WavesAnimation.styled';
+
+const WavesAnimation = () => {
+  return (
+    <>
+      {' '}
+      <Svg
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+        shape-rendering="auto"
+      >
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+          />
+        </defs>
+        <g className="parallax">
+          <use href="#gentle-wave" x="48" y="0" fill="rgba(158, 187, 255,1" />
+
+          <use href="#gentle-wave" x="48" y="5" fill="rgba(236,242,255,0.3)" />
+        </g>
+      </Svg>
+    </>
+  );
+};
+
+export default WavesAnimation;
