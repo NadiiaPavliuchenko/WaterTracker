1ad61f4c19444bd1cd901df4099ff439f4ee4138
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2024-04-02 20:05:38 +0200

    try to fix welc

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -6,11 +6,12 @@ import backgroundDesktop from '../../assets/images/background/WelcomePage/deskto
 import backgroundBubbles from '../../assets/images/background/WelcomePage/desktop/bg_img_bubbles.png';
 
 export const BackgroundWrapper = styled.div`
-  width: 100%;
-  height: 100%;
   background-size: cover;
-  background-position: center;
+  background-position: center bottom;
   background-repeat: no-repeat;
+  background-size: 100%;
+  min-height: 100vh;
+
 
   // мобіл
   background-image: url(${backgroundMobile});
@@ -18,11 +19,13 @@ export const BackgroundWrapper = styled.div`
   // табл
   @media screen and (min-width: 768px) {
     background-image: url(${backgroundTablet});
+    min-height: 100vh;
   }
 
   // деск
   @media screen and (min-width: 1440px) {
     background-image: url(${backgroundDesktop});
+    min-height: 100vh;
   }
 `;
 
@@ -33,11 +36,14 @@ export const Bubble = styled.div`
   background-size: 100%;
   padding-top: 24px;
   padding-bottom: 40px;
+  min-height: calc(100vh - padding-bottom);
 
   // табл
   @media screen and (min-width: 768px) {
     padding-top: 40px;
     padding-bottom: 50px;
+    min-height: calc(100vh - padding-bottom);
+
     background-image: url(${backgroundBubbles});
   }
 
@@ -45,6 +51,8 @@ export const Bubble = styled.div`
   @media screen and (min-width: 1440px) {
     padding-top: 49px;
     padding-bottom: 109px;
+
     background-image: url(${backgroundBubbles});
+    min-height: calc(100vh - padding-bottom);
   }
 `;
