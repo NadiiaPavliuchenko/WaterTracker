abafceab79e17900737723b833e82d5a9dc49169
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-04-03 10:35:05 +0300

    user_logo_and_modal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,13 +1,50 @@
 import UserLogoutModal from 'components/UserLogoutModal/UserLogoutModal';
 import SettingModal from 'components/SettingModal/SettingModal';
+import { useContext, useRef } from 'react';
+import { LogoModalMenu, ModalMenuBtn } from './UserLogoModal.styled';
+import { AnimatePresence } from 'framer-motion';
+import { useModal } from '../../customHooks/useModal';
 
-const UserLogoModal = () => {
+const UserLogoModal = ({ isOpen, onClose }) => {
+  const modalRef = useRef(null);
+  const togModal = useContext(useModal);
+
+  const handleSettingButtonClick = () => {
+    if (onClose && typeof onClose === 'function') {
+      togModal(<SettingModal />);
+      onClose();
+    }
+  };
+
+  const handleLogoutButtonClick = () => {
+    if (onClose && typeof onClose === 'function') {
+      onClose();
+      togModal(
+        <UserLogoutModal
+          onClose={() => {
+            togModal();
+          }}
+        />
+      );
+    }
+  };
+
   return (
-    <>
-      <div>UserLogoModal</div>
-      <SettingModal />
-      <UserLogoutModal />
-    </>
+    <AnimatePresence>
+      {isOpen && (
+        <LogoModalMenu
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          ref={modalRef}
+        >
+          <ModalMenuBtn onClick={handleSettingButtonClick}>
+            Setting
+          </ModalMenuBtn>
+          <ModalMenuBtn onClick={handleLogoutButtonClick}>Log out</ModalMenuBtn>
+        </LogoModalMenu>
+      )}
+    </AnimatePresence>
   );
 };
 
