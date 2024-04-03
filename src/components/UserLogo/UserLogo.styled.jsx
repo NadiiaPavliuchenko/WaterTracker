c3aac61090a318af4186d84b0f0a443a1457ad25
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-04-03 13:12:33 +0300

    user_log_and_mod

---
+++ src/components/UserLogo/UserLogo.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+`;
+
+export const Icon = styled.svg`
+  margin-left: 8px;
+  width: 28px;
+  height: 28px;
+  stroke: rgb(47, 47, 47);
+  fill: transparent;
+`;
