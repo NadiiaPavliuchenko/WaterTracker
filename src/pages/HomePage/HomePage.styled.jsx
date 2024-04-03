6450a03827e009a75bd4b3331248e11c05d29f47
irynatrysh <98460886+irynatrysh@users.noreply.github.com>
2024-04-03 13:04:56 -0400

    updateHomePageCss

--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -3,48 +3,32 @@ import styled from 'styled-components';
 import backgroundBubbles from '../../assets/images/background/WelcomePage/desktop/bg_img_bubbles.png';
 import bottle from '../../assets/images/background/WelcomePage/desktop/bottle_home_screen.png';
 
-export const Wrapper = styled.div`
-background-color: #fff;
-  background-image: url(${backgroundBubbles});
-  background-repeat: no-repeat;
-  background-size: cover;
 
-display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 20px;
-  gap: 20px;
-  max-width: 1440px;
-  margin: 0 auto;
-
-  @media screen and (min-width: 768px) {
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-start;
-  }
-`;
 
 export const HomeSection = styled.div`
-background-image: url(${backgroundBubbles}); 
+  background-image: url(${backgroundBubbles}); 
   background-repeat: no-repeat;
   background-size: cover;
   padding: 0;
-  width: 100%;
+  width: 100vw;
 
- display: flex;
+  display: flex;
   flex-direction: column;
   gap: 20px;
   width: 100%;
 
   @media screen and (min-width: 768px) {
-    /*width: 50%;*/
+    height: 90vh;
     flex-direction: row;
   }
 `;
 
 export const BottleSection = styled.div`
   flex: 1; 
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 
@@ -55,7 +39,7 @@ export const HomeStatistics = styled.div`
   width: 100%;
 
   @media screen and (min-width: 768px) {
-    /*width: 50%;*/
+    height: 90vh;
     flex: 1;
   }
 `;
