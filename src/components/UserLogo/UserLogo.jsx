487a473c5329ea9c0e9fe0961daa1f1d50850fdf
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-04-03 19:05:23 +0300

    add_modalwin

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -26,10 +26,49 @@ const UserLogo = ({ name, email, avatarUrl }) => {
           )}
         </div>
         {isOpenPopup ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-        {isOpenPopup && <UserLogoModal />}
+        {/* {isOpenPopup && <UserLogoModal />} */}
       </ButtonBox>
+      {isOpenPopup && (
+        <UserLogoModal isOpen={isOpenPopup} onClose={() => setIsOpen(false)} />
+      )}
     </>
   );
 };
 
 export default UserLogo;
+
+// import UserLogoModal from 'components/UserLogoModal/UserLogoModal';
+// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+// import { useState } from 'react';
+// import sprite from 'src/assets/sprite.svg';
+// import { ButtonBox, Icon } from './UserLogo.styled';
+
+// const UserLogo = ({ name, email, avatarUrl }) => {
+//   const [isOpenPopup, setIsOpen] = useState(false);
+//   const userName = name ? name : email;
+
+//   const handleClick = () => {
+//     setIsOpen(!isOpenPopup);
+//   };
+//   return (
+//     <>
+//       <ButtonBox onClick={handleClick}>
+//         <div>{userName}</div>
+//         <div>
+//           {avatarUrl ? (
+//             <img src={avatarUrl} alt="avatar" />
+//           ) : (
+//             <Icon>
+//               <use href={`${sprite}#user`}></use>
+//             </Icon>
+//           )}
+//         </div>
+//         {isOpenPopup ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+//         {isOpenPopup && <UserLogoModal />}
+//       </ButtonBox>
+//     </>
+//   );
+// };
+
+// export default UserLogo;
--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,32 +1,30 @@
 import UserLogoutModal from 'components/UserLogoutModal/UserLogoutModal';
 import SettingModal from 'components/SettingModal/SettingModal';
-import { useContext, useRef } from 'react';
+import { useRef, useState } from 'react';
 import { LogoModalMenu, ModalMenuBtn } from './UserLogoModal.styled';
 import { AnimatePresence } from 'framer-motion';
-import useModal from '../../customHooks/useModal';
 
 const UserLogoModal = ({ isOpen, onClose }) => {
   const modalRef = useRef(null);
-  const togModal = useContext(useModal);
+  const [isSettingModalOpen, setIsSettingModalOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
   const handleSettingButtonClick = () => {
-    if (onClose && typeof onClose === 'function') {
-      togModal(<SettingModal />);
-      onClose();
-    }
+    setIsSettingModalOpen(true);
+    onClose && onClose();
   };
 
   const handleLogoutButtonClick = () => {
-    if (onClose && typeof onClose === 'function') {
-      onClose();
-      togModal(
-        <UserLogoutModal
-          onClose={() => {
-            togModal();
-          }}
-        />
-      );
-    }
+    setIsLogoutModalOpen(true);
+    onClose && onClose();
+  };
+
+  const handleSettingModalClose = () => {
+    setIsSettingModalOpen(false);
+  };
+
+  const handleLogoutModalClose = () => {
+    setIsLogoutModalOpen(false);
   };
 
   return (
@@ -44,8 +42,64 @@ const UserLogoModal = ({ isOpen, onClose }) => {
           <ModalMenuBtn onClick={handleLogoutButtonClick}>Log out</ModalMenuBtn>
         </LogoModalMenu>
       )}
+      {isSettingModalOpen && <SettingModal onClose={handleSettingModalClose} />}
+      {isLogoutModalOpen && (
+        <UserLogoutModal onClose={handleLogoutModalClose} />
+      )}
     </AnimatePresence>
   );
 };
 
 export default UserLogoModal;
+
+// import UserLogoutModal from 'components/UserLogoutModal/UserLogoutModal';
+// import SettingModal from 'components/SettingModal/SettingModal';
+// import { useContext, useRef } from 'react';
+// import { LogoModalMenu, ModalMenuBtn } from './UserLogoModal.styled';
+// import { AnimatePresence } from 'framer-motion';
+// import useModal from '../../customHooks/useModal';
+
+// const UserLogoModal = ({ isOpen, onClose }) => {
+//   const modalRef = useRef(null);
+//   const togModal = useContext(useModal);
+
+//   const handleSettingButtonClick = () => {
+//     if (onClose && typeof onClose === 'function') {
+//       togModal(<SettingModal />);
+//       onClose();
+//     }
+//   };
+
+//   const handleLogoutButtonClick = () => {
+//     if (onClose && typeof onClose === 'function') {
+//       onClose();
+//       togModal(
+//         <UserLogoutModal
+//           onClose={() => {
+//             togModal();
+//           }}
+//         />
+//       );
+//     }
+//   };
+
+//   return (
+//     <AnimatePresence>
+//       {isOpen && (
+//         <LogoModalMenu
+//           initial="initial"
+//           animate="animate"
+//           exit="exit"
+//           ref={modalRef}
+//         >
+//           <ModalMenuBtn onClick={handleSettingButtonClick}>
+//             Setting
+//           </ModalMenuBtn>
+//           <ModalMenuBtn onClick={handleLogoutButtonClick}>Log out</ModalMenuBtn>
+//         </LogoModalMenu>
+//       )}
+//     </AnimatePresence>
+//   );
+// };
+
+// export default UserLogoModal;
