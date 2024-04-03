8667d8fbc1479bb4a0b5471d7ddb4d5689f02489
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-04-03 12:50:50 +0300

    user_log_and_mod

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import SignUpForm from 'components/SignUpForm/SignUpForm';
 import { signUpAPI } from '../../store/auth/authOperations';
 import { SignUpPageStyle } from './SignupPage.styled';
 import { getUserEmail } from '../../store/auth/authSelectors';
 import Wrapper from '../../components/Wrapper/Wrapper';
+import bottleImage_mob_1x from '../../assets/images/background/RegisterLoginPage/Desk/bottle_desk_2x.png';
 
 const SignupPage = () => {
   const dispatch = useDispatch();
@@ -13,18 +13,25 @@ const SignupPage = () => {
   function handleSubmit(values) {
     dispatch(signUpAPI(values));
   }
+
   return (
     <>
-      <Wrapper>
-        <SignUpPageStyle>
-          <h2 className="title">Sign Up</h2>
-          <SignUpForm submitFunc={handleSubmit} />
-          <Link className="link" to="/signin">
-            Sign In
-          </Link>
-          {userEmail ? <Navigate to="/signin" /> : <Navigate to="/signup" />}
-        </SignUpPageStyle>
-      </Wrapper>
+      <SignUpPageStyle>
+        <Wrapper>
+          <div className="formCont">
+            <h2 className="title">Sign Up</h2>
+            <SignUpForm submitFunc={handleSubmit} />
+            <Link className="link" to="/signin">
+              Sign In
+            </Link>
+          </div>
+          <img
+            className="bottle"
+            alt="bottle of water"
+            src={bottleImage_mob_1x}
+          ></img>
+        </Wrapper>
+      </SignUpPageStyle>
     </>
   );
 };
