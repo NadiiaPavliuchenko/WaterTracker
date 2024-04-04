d987bd4a0c6609c6861b6d80d84178822ed540fe
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-04-04 12:41:44 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/WaterTrackerFrontend@428174443ee61893b8f00109c4a1029f9e03c536 🚀

--- assets/ErrorPage-d46b3d4c.js
+++ assets/ErrorPage-d46b3d4c.js
@@ -1,4 +1,4 @@
-import{b as t,j as e,H as n}from"./index-8ece4478.js";const b=t.div`
+import{b as t,j as e,H as n}from"./index-6027f88a.js";const b=t.div`
   height: 100%;
   display: flex;
   justify-content: center;
--- assets/SigninPage-2ebb171e.js
+++ assets/SigninPage-2ebb171e.js
@@ -1,4 +1,4 @@
-import{b as d,B as h,C as r,j as e,F as m,D as g,E as p,G as l,z as a,u as f,W as b,H as u,I as w}from"./index-8ece4478.js";import{b as j,a as y,c as S,d as k}from"./bottle_desk_2x-0e16256e.js";const N=d.div`
+import{b as d,B as h,C as r,j as e,F as m,D as g,E as p,G as l,z as a,u as f,W as b,H as u,I as w}from"./index-6027f88a.js";import{b as j,a as y,c as S,d as k}from"./bottle_desk_2x-0e16256e.js";const N=d.div`
 @media screen and (min-width: 320px) {
 .form {
     margin-top: 16px;
@@ -91,7 +91,7 @@ import{b as d,B as h,C as r,j as e,F as m,D as g,E as p,G as l,z as a,u as f,W a
     }
 }
 
-`,v=({submitFunc:s})=>{const n=h({email:r().email().required(),password:r().min(6).max(64).required()}),o={email:"",password:""};function c(i,t){s(i),t.resetForm()}function x(i){const t=i.target.closest(".watchPasswordIcon").previousSibling;if(t.type==="password"){t.type="text",i.target.closest("SVG").firstChild.href.baseVal=a+"#eye-opened";return}if(t.type==="text"){t.type="password",i.target.closest("SVG").firstChild.href.baseVal=a+"#eye-closed";return}}return e.jsx(e.Fragment,{children:e.jsx(N,{children:e.jsx(m,{initialValues:o,validationSchema:n,onSubmit:c,children:e.jsxs(g,{className:"form",autoComplete:"off",children:[e.jsxs("label",{className:"label",htmlFor:"email",children:["Enter your email",e.jsx("br",{}),e.jsx("div",{className:"input-container",children:e.jsx(p,{className:"input",type:"text",name:"email",placeholder:"Email"})}),e.jsx(l,{className:"error",name:"email",component:"div"})]}),e.jsx("br",{}),e.jsxs("label",{className:"label",htmlFor:"password",children:["Enter your password",e.jsxs("div",{className:"input-container",children:[e.jsx(p,{className:"input",type:"password",name:"password",placeholder:"Password"}),e.jsx("svg",{className:"watchPasswordIcon",onClick:x,width:"16",height:"14",stroke:"#9ebbff",fill:"none",children:e.jsx("use",{href:a+"#eye-closed"})})]}),e.jsx(l,{className:"error",name:"password",component:"div"})]}),e.jsx("br",{}),e.jsx("button",{className:"buttonSignUp",type:"submit",children:"Sign In"})]})})})})},F=d.div`
+`,v=({submitFunc:s})=>{const o=h({email:r().email().required(),password:r().min(6).max(64).required()}),n={email:"",password:""};function c(i,t){s(i),t.resetForm()}function x(i){const t=i.target.closest(".watchPasswordIcon").previousSibling;if(t.type==="password"){t.type="text",i.target.closest("SVG").firstChild.href.baseVal=a+"#eye-opened";return}if(t.type==="text"){t.type="password",i.target.closest("SVG").firstChild.href.baseVal=a+"#eye-closed";return}}return e.jsx(e.Fragment,{children:e.jsx(N,{children:e.jsx(m,{initialValues:n,validationSchema:o,onSubmit:c,children:e.jsxs(g,{className:"form",autoComplete:"off",children:[e.jsxs("label",{className:"label",htmlFor:"email",children:["Enter your email",e.jsx("br",{}),e.jsx("div",{className:"input-container",children:e.jsx(p,{className:"input",type:"text",name:"email",placeholder:"Email"})}),e.jsx(l,{className:"error",name:"email",component:"div"})]}),e.jsx("br",{}),e.jsxs("label",{className:"label",htmlFor:"password",children:["Enter your password",e.jsxs("div",{className:"input-container",children:[e.jsx(p,{className:"input",type:"password",name:"password",placeholder:"Password"}),e.jsx("svg",{className:"watchPasswordIcon",onClick:x,width:"16",height:"14",stroke:"#9ebbff",fill:"none",children:e.jsx("use",{href:a+"#eye-closed"})})]}),e.jsx(l,{className:"error",name:"password",component:"div"})]}),e.jsx("br",{}),e.jsx("button",{className:"buttonSignUp",type:"submit",children:"Sign In"})]})})})})},F=d.div`
 background-size: 100vw, contain;
 background-position: 0, 10px ;
 background-repeat: no-repeat;
@@ -118,7 +118,7 @@ line-height: 1.25;
 color: #407bff;
 }
 .bottle {
-    margin-top: 40px;
+    // margin-top: 40px;
     position: relative;
     width: 280px;
     z-index: -10;
@@ -126,6 +126,10 @@ color: #407bff;
 }
 @media screen and (min-width: 768px) {
     background-image: url(${y});
+    .wrapper {
+        width: 100%;
+        position: relative;
+    }
     .bottle {    
         width: 736px; 
         position: absolute;
@@ -142,13 +146,17 @@ color: #407bff;
     .bottle {    
         width: 916px; 
         position: absolute;
-        top: 80px;
-        left: -51px;
+        top: 30px;
+        left: -100px;
     }
     .formCont {
         position: absolute;
-        top: 200px;
-        right: 216px;
+        top: 120px;
+        right: 90px;
+    }
+    .wrapper {
+        width: 100%;
+        position: relative;
     }
 }
-`,I=()=>{const s=f();function n(o){s(w(o))}return e.jsx(e.Fragment,{children:e.jsx(F,{children:e.jsxs(b,{children:[e.jsxs("div",{className:"formCont",children:[e.jsx("div",{className:"title",children:"SigninPage"}),e.jsx(v,{submitFunc:n}),e.jsx(u,{className:"link",to:"/signup",children:"Sign Up"})]}),e.jsx("img",{className:"bottle",alt:"bottle of water",src:k})]})})})};export{I as default};
+`,I=()=>{const s=f();function o(n){s(w(n))}return e.jsx(e.Fragment,{children:e.jsx(F,{children:e.jsx(b,{children:e.jsxs("div",{className:"wrapper",children:[e.jsxs("div",{className:"formCont",children:[e.jsx("h2",{className:"title",children:"SigninPage"}),e.jsx(v,{submitFunc:o}),e.jsx(u,{className:"link",to:"/signup",children:"Sign Up"})]}),e.jsx("img",{className:"bottle",alt:"bottle of water",src:k})]})})})})};export{I as default};
