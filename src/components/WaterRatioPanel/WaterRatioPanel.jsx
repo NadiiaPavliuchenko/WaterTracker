d87873bac08d2796f25a57fe58c3c82540bc4758
roLLlyTHuK <ircvipua@gmail.com>
2024-04-04 11:12:44 +0300

    upd progress

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -6,10 +6,15 @@ import {
   ProgressContainer,
 } from './WaterRatioPanel.styled';
 import ControlPointRoundedIcon from '@mui/icons-material/ControlPointRounded';
+import { useSelector } from 'react-redux';
+import { getCurrentPercentage } from '../../store/water/waterSelectors';
 
 const WaterRatioPanel = () => {
   const [openList, setOpenList] = useState(false);
+  // const progressPercentage = useSelector(getCurrentPercentage);
+  const progressPercentage = 70;
 
+  console.log('progressPercentage :>> ', progressPercentage);
   const handleOpenClick = () => {
     setOpenList(true);
   };
@@ -17,14 +22,36 @@ const WaterRatioPanel = () => {
   const handleCloseClick = () => {
     setOpenList(false);
   };
-
+  const progress = progressPercentage > 100 ? 100 : progressPercentage;
   return (
     <Container>
+      {/* Progress Bar */}
       <ProgressContainer>
-        <p>Title</p>
-        <progress value="0" min="0" step="1" max="100" />
+        <h3>Today</h3>
+        <div className="progress-bar-container">
+          <div className="progress-bar">
+            <div className="progress" style={{ width: `${progress}%` }}>
+              <p className="progress-text">
+                {' '}
+                {progressPercentage > 100
+                  ? `${progressPercentage}%`
+                  : `${progress}%`}
+              </p>
+            </div>
+            <div className="marker-0">0%</div>
+            <div className="marker-50">50%</div>
+            <div className="marker-100">100%</div>
+            <div
+              className="indicator"
+              style={{
+                left: `${progress}%`,
+              }}
+            ></div>
+          </div>
+        </div>
       </ProgressContainer>
 
+      {/* Add Water Button */}
       <AddWaterBtn onClick={handleOpenClick}>
         <ControlPointRoundedIcon
           sx={{ fill: 'var(--primary-white)', width: '24px', height: '24px' }}
--- src/components/WaterRatioPanel/WaterRatioPanel.styled.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.styled.jsx
@@ -2,24 +2,96 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  height: 90px;
   flex-direction: row;
   align-items: center;
   gap: 23px;
 `;
 
 export const ProgressContainer = styled.div`
-  flex: 2;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: center;
+  /* justify-content: center; */
+  height: 100%;
 
-  p {
-    font-family: Roboto;
+  h3 {
+    font-family: inherit;
     font-size: 18px;
     font-weight: 400;
     line-height: 24px;
     text-align: center;
+    margin-bottom: 18px;
+    color: var(--primary-blue);
+  }
+
+  .progress-bar-container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .progress-bar {
+    position: relative;
+    width: 340px;
+    height: 8px;
+    background-color: rgba(215, 227, 255, 1);
+  }
+
+  .progress {
+    position: relative;
+    height: 100%;
+    background-color: rgba(158, 187, 255, 1);
+    text-align: center;
+    line-height: 20px;
+    font-size: 12px;
+  }
+
+  .progress-text {
+    position: absolute;
+    top: -200%;
+    left: 100%;
+    transform: translate(-50%, -50%);
+    font-size: 20px;
+    color: var(--primary-blue);
+  }
+
+  .marker-0,
+  .marker-50,
+  .marker-100 {
+    position: absolute;
+    bottom: -20px;
+    font-size: 12px;
+    color: #666666;
+  }
+  .marker-100 {
+    right: -50px;
+  }
+
+  .marker-0 {
+    left: 0;
+  }
+
+  .marker-50 {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .marker-100 {
+    right: 0;
+    transform: translateX(50%);
+  }
+
+  .indicator {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 14px;
+    height: 14px;
+    background-color: rgba(255, 255, 255, 1); /* Цвет бегунка */
+    border: 1px solid rgba(64, 123, 255, 1);
+    border-radius: 50%;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
   }
 `;
 
