75e2e85904d322293520e2b89855c13489514604
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-04-04 16:49:08 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/WaterTrackerFrontend@8955d3094b57e030c9059219e08df3517da5227a 🚀

--- assets/ErrorPage-cfc3a022.js
+++ assets/ErrorPage-cfc3a022.js
@@ -1,4 +1,4 @@
-import{u as t,j as e,I as n}from"./index-341be3ca.js";const b=t.div`
+import{u as t,j as e,I as n}from"./index-1376f19c.js";const b=t.div`
   height: 100%;
   display: flex;
   justify-content: center;
