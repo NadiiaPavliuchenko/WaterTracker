c15d33e77ed85c7b3019bd6a4e54c397dc806e8a
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-04-05 20:07:15 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/WaterTrackerFrontend@571389d943f6ed9691ca8cd4b0c75db14dd85679 🚀

--- assets/ErrorPage-dd8951f8.js
+++ assets/ErrorPage-dd8951f8.js
@@ -1,4 +1,4 @@
-import{u as t,j as e,J as n}from"./index-e801ac8b.js";const b=t.div`
+import{u as t,j as e,J as n}from"./index-8023ae5f.js";const b=t.div`
   height: 100%;
   display: flex;
   justify-content: center;
