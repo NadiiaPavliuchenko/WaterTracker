a9791abccd9159ad1f24ebcfd833ee003ee5d190
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-04-05 20:39:09 +0300

    daily_styles

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { getCurrentUser } from '../../store/auth/authSelectors';
 import { useState } from 'react';
 import { DailyNormaModal } from '../DailyNormaModal/DailyNormaModal';
+import { ModalDaily, DailyTitle, DailyBtn, DailyLitr, DailyEditContainer } from './DailyNorma.styled';
 
 const DailyNorma = () => {
   const { waterRate } = useSelector(getCurrentUser);
@@ -19,16 +20,16 @@ const DailyNorma = () => {
   const waterRateL = (waterRate / 1000).toFixed(1) + ' L';
 
   return (
-    <div>
-      <div>
-        <h2>My daily norma: </h2>
-        <div>
-          <p>{waterRateL}</p>
-          <button onClick={openModal}>Edit</button>
-        </div>
+   
+      <ModalDaily>
+        <DailyTitle>My daily norma: </DailyTitle>
+        <DailyEditContainer>
+          <DailyLitr>{waterRateL}</DailyLitr>
+          <DailyBtn onClick={openModal}>Edit</DailyBtn>
+        </DailyEditContainer>
         <DailyNormaModal isModalOpen={isModalOpen} closeModal={closeModal} />
-      </div>
-    </div>
+      </ModalDaily>
+   
   );
 };
 
