49e78b81a87d33cbb9de5075b5428e9ab6a4facf
andyfullstack <andriikoliadenko@gmail.com>
2024-04-05 16:57:23 +0300

    upd fix dispach

--- src/components/Calendar/Month/Month.jsx
+++ src/components/Calendar/Month/Month.jsx
@@ -33,13 +33,13 @@ export const Calendar = (dailyNormaState) => {
   const waterForMonth = useSelector(getCurrentMonth);
   const isLoading = useSelector(getIsDayDataLoading);
 
-  // useEffect(() => {
-  //   const month = `${
-  //     currentDate.getMonth() + 1
-  //   } - ${currentDate.getFullYear()}`;
+  useEffect(() => {
+    const month = `${
+      currentDate.getMonth() + 1
+    } - ${currentDate.getFullYear()}`;
 
-  //   dispatch(getCurrentMonthInfoThunk(month));
-  // }, [dispatch, currentDate, dailyNormaState]);
+    dispatch(getCurrentMonthInfoThunk(month));
+  }, [dispatch, currentDate, dailyNormaState]);
 
   // ===============================================================
 
@@ -48,33 +48,32 @@ export const Calendar = (dailyNormaState) => {
 
   // ============================================================================
 
-  useEffect(() => {
-    // Получаем первый и последний день текущего месяца
-    const firstDayOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      1
-    );
-    const lastDayOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth() + 1,
-      0
-    );
+  // useEffect(() => {
+  //   // Получаем первый и последний день текущего месяца
+  //   const firstDayOfMonth = new Date(
+  //     currentDate.getFullYear(),
+  //     currentDate.getMonth(),
+  //     1
+  //   );
+  //   const lastDayOfMonth = new Date(
+  //     currentDate.getFullYear(),
+  //     currentDate.getMonth() + 1,
+  //     0
+  //   );
 
-    // lastDayOfMonth.setDate(lastDayOfMonth.getDate() + 1);
-    // Формируем строку для запроса, содержащую начальную и конечную дату месяца
-    const startDate = firstDayOfMonth.toISOString().split('T')[0];
-    const endDate = lastDayOfMonth.toISOString().split('T')[0];
-    // const dateRange = `${startDate} , ${endDate}`;
-    const dateRange = {
-      startDate,
-      endDate,
-    };
+  //   // Формируем строку для запроса, содержащую начальную и конечную дату месяца
+  //   const startDate = firstDayOfMonth.toISOString().split('T')[0];
+  //   const endDate = lastDayOfMonth.toISOString().split('T')[0];
+  //   // const dateRange = `${startDate} , ${endDate}`;
+  //   const dateRange = {
+  //     startDate,
+  //     endDate,
+  //   };
 
-    // Вызываем thunk, передавая в него начальную и конечную дату месяца
-    dispatch(getCurrentMonthInfoThunk(dateRange));
-    console.log(dateRange);
-  }, [dispatch, currentDate, dailyNormaState]);
+  //   // Вызываем thunk, передавая в него начальную и конечную дату месяца
+  //   dispatch(getCurrentMonthInfoThunk(dateRange));
+  //   console.log(dateRange);
+  // }, [dispatch, currentDate, dailyNormaState]);
 
   // =========================================================================
 
@@ -121,11 +120,10 @@ export const Calendar = (dailyNormaState) => {
     const daysInMonth = getDaysInMonth();
     return Array.from({ length: daysInMonth }, (_, index) => {
       const day = index + 1;
-      const consumedWaterPercentag =
-        waterForMonth?.find(
-          (item) =>
-            item.dayOfMonth && Number(item.dayOfMonth.split(',')[0]) === day
-        )?.consumedWaterPercentage || 0;
+      const waterPercentage = waterForMonth?.find(
+        (item) =>
+          item.dayOfMonth && Number(item.dayOfMonth.split(',')[0]) === day
+      );
 
       return (
         <DayComponent
@@ -134,7 +132,7 @@ export const Calendar = (dailyNormaState) => {
           day={day}
           //TODO: вставить процентаж
 
-          consumedWaterPercentag={consumedWaterPercentag}
+          consumedWaterPercentag={waterPercentage}
         />
       );
     });
