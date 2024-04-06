af5c307c331b5bfe225b04f3ef8342645b89a5f7
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-04-06 18:24:58 +0300

    corr

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -38,8 +38,6 @@ const UserLogoModal = ({ isOpen }) => {
             </Icon>
             Log out
           </ModalMenuBtn>
-
-          
         </LogoModalMenu>
       )}
       {activeModal === 'settings' && (
@@ -53,62 +51,3 @@ const UserLogoModal = ({ isOpen }) => {
 };
 
 export default UserLogoModal;
-
-
-
-
-
-// import UserLogoutModal from 'components/UserLogoutModal/UserLogoutModal';
-// import SettingModal from 'components/SettingModal/SettingModal';
-// import { useRef, useState } from 'react';
-// import { LogoModalMenu, ModalMenuBtn, Icon } from './UserLogoModal.styled';
-// import { AnimatePresence } from 'framer-motion';
-// import sprite from 'src/assets/sprite.svg';
-
-// const UserLogoModal = ({ isOpen }) => {
-//   const modalRef = useRef(null);
-//   const [activeModal, setActiveModal] = useState(null);
-
-//   const handleModalOpen = (modal) => {
-//     setActiveModal(modal);
-//   };
-
-//   const handleModalClose = () => {
-//     setActiveModal(null);
-//   };
-
-//   return (
-//     <AnimatePresence>
-//       {isOpen && (
-//         <LogoModalMenu
-//           initial="initial"
-//           animate="animate"
-//           exit="exit"
-//           ref={modalRef}
-//         >
-//           <ModalMenuBtn onClick={() => handleModalOpen('settings')}>
-//             <Icon>
-//               <use href={`${sprite}#settings`}></use>
-//             </Icon>
-//             Settings
-//           </ModalMenuBtn>
-//           <ModalMenuBtn onClick={() => handleModalOpen('logout')}>
-//             <Icon>
-//               <use href={`${sprite}#arrow-right-on-rectangle`}></use>
-//             </Icon>
-//             Log out
-//           </ModalMenuBtn>
-//         </LogoModalMenu>
-//       )}
-//       {activeModal === 'settings' && (
-//         <SettingModal onModalClose={handleModalClose} isModalOpen={true} />
-//       )}
-//       {activeModal === 'logout' && (
-//         <UserLogoutModal onModalClose={handleModalClose} isModalOpen={true} />
-//       )}
-//     </AnimatePresence>
-//   );
-// };
-
-// export default UserLogoModal;
-
