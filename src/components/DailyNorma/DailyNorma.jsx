2f295fed2dab0c968be556f6c529de1cf3752099
roLLlyTHuK <ircvipua@gmail.com>
2024-04-06 17:58:42 +0300

    fix visual norm

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -2,10 +2,16 @@ import { useSelector } from 'react-redux';
 import { getCurrentUser } from '../../store/auth/authSelectors';
 import { useState } from 'react';
 import { DailyNormaModal } from '../DailyNormaModal/DailyNormaModal';
-import { ModalDaily, DailyTitle, DailyBtn, DailyLitr, DailyEditContainer } from './DailyNorma.styled';
+import {
+  ModalDaily,
+  DailyTitle,
+  DailyBtn,
+  DailyLitr,
+  DailyEditContainer,
+} from './DailyNorma.styled';
 
 const DailyNorma = () => {
-  const { waterRate } = useSelector(getCurrentUser);
+  const { dailyWaterGoal } = useSelector(getCurrentUser);
   const [isModalOpen, setModalOpen] = useState(false);
 
   const openModal = () => {
@@ -17,19 +23,17 @@ const DailyNorma = () => {
   };
 
   // Перетворення мл у л
-  const waterRateL = (waterRate / 1000).toFixed(1) + ' L';
+  const dailyWaterGoalL = (dailyWaterGoal / 1000).toFixed(1) + ' L';
 
   return (
-   
-      <ModalDaily>
-        <DailyTitle>My daily norma: </DailyTitle>
-        <DailyEditContainer>
-          <DailyLitr>{waterRateL}</DailyLitr>
-          <DailyBtn onClick={openModal}>Edit</DailyBtn>
-        </DailyEditContainer>
-        <DailyNormaModal isModalOpen={isModalOpen} closeModal={closeModal} />
-      </ModalDaily>
-   
+    <ModalDaily>
+      <DailyTitle>My daily norma: </DailyTitle>
+      <DailyEditContainer>
+        <DailyLitr>{dailyWaterGoalL}</DailyLitr>
+        <DailyBtn onClick={openModal}>Edit</DailyBtn>
+      </DailyEditContainer>
+      <DailyNormaModal isModalOpen={isModalOpen} closeModal={closeModal} />
+    </ModalDaily>
   );
 };
 
