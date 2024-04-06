5b699ffecc36c83ffafd152efed76ec59b065467
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-04-06 15:50:52 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/WaterTrackerFrontend@6361aba92bb84b655ed7186a53fcb7439e4cf7ff 🚀

--- assets/ErrorPage-5dfd825d.js
+++ assets/ErrorPage-5dfd825d.js
@@ -1,4 +1,4 @@
-import{u as e,j as t,K as n}from"./index-07dc369e.js";import{B as o}from"./BubblesAnimation-c96c4cef.js";const b=e.div`
+import{u as e,j as t,K as n}from"./index-75879f05.js";import{B as o}from"./BubblesAnimation-536b7d77.js";const b=e.div`
   height: 100%;
   display: flex;
   justify-content: center;
--- assets/SigninPage-32ed128d.js
+++ assets/SigninPage-32ed128d.js
@@ -1,4 +1,4 @@
-import{u as x,D as h,E as p,j as e,F as m,G as f,H as d,I as c,A as l,a as g,M as b,J as u,r as w,W as j,K as y,N as v}from"./index-07dc369e.js";import{b as k,a as S,c as N,d as R}from"./bottle_desk_2x-0e16256e.js";const F=x.div`
+import{u as x,D as h,E as p,j as e,F as m,G as f,H as d,I as c,A as l,a as g,M as b,J as u,r as w,W as j,K as y,N as v}from"./index-75879f05.js";import{b as k,a as S,c as N,d as R}from"./bottle_desk_2x-0e16256e.js";const F=x.div`
 @media screen and (min-width: 320px) {
 .form {
     margin-top: 16px;
