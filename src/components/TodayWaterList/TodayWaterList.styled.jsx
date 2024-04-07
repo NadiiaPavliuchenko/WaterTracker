c8da363c9fc7abb1fd6de413083a3cb7215d8ec8
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-04-08 02:20:58 +0300

    add theme to addWaterModal

--- src/components/TodayWaterList/TodayWaterList.styled.jsx
+++ src/components/TodayWaterList/TodayWaterList.styled.jsx
@@ -1,115 +1,104 @@
 import styled from 'styled-components';
 
 export const WaterTodayListStyle = styled.div`
+  *::-webkit-scrollbar {
+    width: 4px;
+  }
+  *::-webkit-scrollbar-track {
+    background: #d7e3ff;
+  }
+  *::-webkit-scrollbar-thumb {
+    background-color: blue;
+    border-radius: 8px;
+    border: 3px solid #9ebbff;
+  }
 
-*::-webkit-scrollbar {
-   width: 4px; 
-} 
-*::-webkit-scrollbar-track {
-   background: #d7e3ff; 
-} 
-*::-webkit-scrollbar-thumb {
-   background-color: blue;
-   border-radius: 8px;
-   border: 3px solid #9ebbff;
-}
-
-
-@media screen and (min-width: 320px) {
-    
+  @media screen and (min-width: 320px) {
     background-color: #ecf2ff;
 
     min-width: 280 px;
-    
+
     .title {
-        font-family: "Roboto", sans-serif;
-        font-weight: 500;
-        font-size: 24px;
-        line-height: 1.25;
-        color: #2f2f2f;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 500;
+      font-size: 24px;
+      line-height: 1.25;
+      color: #2f2f2f;
     }
     .waterList {
-        overflow-y: scroll;
-        // scrollbar-width: 5px;
-        // scrollbar-color: #9ebbff #9ebbff;
-        
+      overflow-y: scroll;
+      // scrollbar-width: 5px;
+      // scrollbar-color: #9ebbff #9ebbff;
 
-        display: flex;
-        justify-content: space-between;
-        flex-direction: column;
-        gap: 12px;
-        align-items: center;
-        
-        border-radius: 10px;
-        padding: 24px 8px;
-        width: 100%;
-        max-height: 200px;
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      gap: 12px;
+      align-items: center;
+
+      border-radius: 10px;
+      padding: 24px 8px;
+      width: 100%;
+      max-height: 200px;
     }
-    
+
     .waterItem {
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        border-bottom: 1px solid #d7e3ff;
-        align-items: center;
-        // gap: 38px;
-        // padding: 12px 0;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      border-bottom: 1px solid #d7e3ff;
+      align-items: center;
+      // gap: 38px;
+      // padding: 12px 0;
     }
-    
-    .volumeAdnDate {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        gap: 12px;
 
+    .volumeAdnDate {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      gap: 12px;
     }
     .waterGlass {
-        width=26px;
-        height=26px;
+      width: 26px;
+      height: 26px;
     }
     .waterVolume {
-        display: flex;
-        justify-content: end;
-        min-width: 60px;
-        font-family: "Roboto", sans-serif;
-        font-weight: 400;
-        font-size: 18px;
-        line-height: 1.33333;
-        color: #407bff;
+      display: flex;
+      justify-content: end;
+      min-width: 60px;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 400;
+      font-size: 18px;
+      line-height: 1.33333;
+      color: #407bff;
     }
     .waterTime {
-        font-family: "Roboto", sans-serif;
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 2;
-        color: #2f2f2f;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 2;
+      color: #2f2f2f;
     }
     .editDelete {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        gap: 18px;
-
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      gap: 18px;
     }
     .addWater {
-        font-family: "Roboto", sans-serif;
-        font-weight: 500;
-        font-size: 16px;
-        line-height: 1.25;
-        color: #407bff;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 1.25;
+      color: #407bff;
     }
-}
- @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 768px) {
     .waterList {
-        width: 100%;
-
+      width: 100%;
     }
-   
-
- }
-
-
-`
+  }
+`;
