62d25827805b29fecd9e0a5427704c8c02da762c
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-04-07 22:45:55 +0300

    new_position

--- src/components/UserLogo/UserLogo.styled.jsx
+++ src/components/UserLogo/UserLogo.styled.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const ButtonBox = styled.div`
+position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -32,6 +33,43 @@ export const Icon = styled.svg`
   stroke: ${({ theme }) => theme.color.blue};
   fill: transparent;
 `;
+
+
+// import styled from 'styled-components';
+
+// export const ButtonBox = styled.div`
+
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 8px;
+//   cursor: pointer;
+
+//   &:hover,
+//   &:focus {
+//     cursor: pointer;
+//   }
+// `;
+
+// export const AvatarStile = styled.div`
+//   width: 28px;
+//   height: 28px;
+//   overflow: hidden;
+//   border-radius: 50%;
+// `;
+
+// export const IconStyle = styled.img`
+//   width: 100%;
+//   height: 100%;
+// `;
+
+// export const Icon = styled.svg`
+//   margin-left: 8px;
+//   width: 28px;
+//   height: 28px;
+//   stroke: ${({ theme }) => theme.color.blue};
+//   fill: transparent;
+// `;
 
 // export const KeyboardArrowUpIcon = styled.buttonBox`
 //   transform: ${(props) =>
