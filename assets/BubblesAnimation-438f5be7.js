ba995e898e6a90b926ecb8eb92f519e4649f7bba
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-04-08 09:11:01 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/WaterTrackerFrontend@07cf03e49a86d2a3af8b59d1dae4ac8cfd2173f4 🚀

--- assets/BubblesAnimation-438f5be7.js
+++ assets/BubblesAnimation-438f5be7.js
@@ -1,4 +1,4 @@
-import{u as t,j as p}from"./index-cb750b3f.js";const e=t.div`
+import{u as t,j as p}from"./index-f3f2b24e.js";const e=t.div`
   position: fixed;
   display: block;
   width: 327.22px;
