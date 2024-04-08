d60d7078eebc00dc11af2d5da9b5cfe3a1688367
NadiiaPavliuchenko <72461487+NadiiaPavliuchenko@users.noreply.github.com>
2024-04-08 12:39:28 +0000

    Deploying to gh-pages from @ NadiiaPavliuchenko/WaterTrackerFrontend@f8a1fea91743338226cd9484cb666b694c55e031 🚀

--- assets/ErrorPage-7c051f43.js
+++ assets/ErrorPage-7c051f43.js
@@ -1,4 +1,4 @@
-import{u as o,j as i,L as e}from"./index-757d396b.js";import{B as n}from"./BubblesAnimation-ebeb4e26.js";const r=o.div`
+import{u as o,j as i,L as e}from"./index-0c61198a.js";import{B as n}from"./BubblesAnimation-cd2b6768.js";const r=o.div`
   height: 100%;
   display: flex;
   justify-content: center;
