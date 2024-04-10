2fdb7c46cef489b294f6c9728c40a8f04a29766b
roLLlyTHuK <ircvipua@gmail.com>
2024-04-10 12:56:27 +0300

    4

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -104,7 +104,7 @@ const TeamPage = () => {
   return (
     <Wrapper>
       <BubblesAnimation />
-      <Title>Our team IT Froggs</Title>
+      <Title>Our team &#171; IT Froggs &#187;</Title>
       <Container>
         <ul className="members">
           {teamMembers.map((member, index) => (
