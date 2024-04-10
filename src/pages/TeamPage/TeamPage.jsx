f63edeecc5140c420d94b2072cb4824b826776e5
roLLlyTHuK <ircvipua@gmail.com>
2024-04-10 11:07:21 +0300

    refactor

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,5 +1,3 @@
-import Wrapper from 'components/Wrapper/Wrapper';
-import { Container, Title } from './TeamPage.styled';
 import AD from '../../assets/images/team/AD.png';
 import AK from '../../assets/images/team/AK.png';
 import BS from '../../assets/images/team/BS.png';
@@ -10,240 +8,246 @@ import NP from '../../assets/images/team/NP.png';
 import OP from '../../assets/images/team/OP.png';
 import SK from '../../assets/images/team/SK.png';
 import SO from '../../assets/images/team/SO.png';
-import NoFoto from '../../assets/images/team/nofoto.png';
 import VS from '../../assets/images/team/VS.png';
+import { Container, Title } from './TeamPage.styled';
+import Wrapper from '../../components/Wrapper/Wrapper';
+import BubblesAnimation from '../../components/Animation/BubblesAnimation';
 
 const TeamPage = () => {
   return (
     <Wrapper>
+      <BubblesAnimation />
+
       <Title>Our team IT Froggs</Title>
       <Container>
-        <div className="front-end">
-          <h2 className="front-end-title">Frontend</h2>
-          <ul className="membersFront">
-            <li className="member">
-              <a
-                href="www.linkedin.com/in/nadiia-pavliuchenko"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={NP} alt="Nadiia Pavliuchenko" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Nadiia Pavliuchenko, <span>TechLeader, Developer</span>
-                  </h3>
-                  <p>
-                    Developed part: deployment of a server for development,
-                    routes, SettingsModal, implementation of dark and light
-                    themes
-                  </p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/andrii-dovhaniuk-ua/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={AD} alt="Andrii Dovhaniuk" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Andrii Dovhaniuk, <span>SCRUM, Developer</span>
-                  </h3>
-                  <p>
-                    Developed part: redux state, api thunks, addWaterModal,
-                    animation, loader, 404 page, deleteUserModal/Form,
-                    modalContainer, wrapper, recoveryModal/Form,
-                    resendCodeModal/Form, userLogoutModal, waterRatioPanel,
-                    notifications, services, help with complex issues.
-                  </p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/andrii-koliadenko/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={AK} alt="Andrii Koliadenko" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Andrii Koliadenko, <span>Developer</span>
-                  </h3>
-                  <p>Developed part: Calendar, TodayListModal</p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href=" https://www.linkedin.com/in/ivan-imenynnyk/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={II} alt="Ivan Imenynnyk" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Ivan Imenynnyk, <span>Developer</span>
-                  </h3>
-                  <p>Developed part: DailyNormaModal</p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/irynatrysh/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={IT} alt="Iryna Trysh" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Iryna Trysh, <span>Developer</span>
-                  </h3>
-                  <p>Developed part: HomePage</p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/bogdan-solomich/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={BS} alt="Bohdan Solomych" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Bohdan Solomych, <span>Developer</span>
-                  </h3>
-                  <p>Developed part: Header, UserLogo</p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/dana-dobryshkina-ua"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={DD} alt="Dana Dobryshkina" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Dana Dobryshkina, <span>Developer</span>
-                  </h3>
-                  <p>
-                    Developed part: WelcomePage, WhyDrinkWater, Main,
-                    WaterTrackerConsumption, partially DailyNorma
-                  </p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/volodymyr-shevchenko-developer/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={VS} alt=" Volodymyr Shevchenko" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Volodymyr Shevchenko, <span>Developer</span>
-                  </h3>
-                  <p>
-                    Developed part: SignInPage, SignUpPage, SignIn/SignUp form,
-                    TodayWaterList, DeleteWaterIntakesModal
-                  </p>
-                </div>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="back-end">
-          <h2 className="back-end-title">Backend</h2>
-          <ul className="membersBack">
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/svitlana-otenko/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={SO} alt="Svitlana Otenko" />
-                </div>
-                <div className="desc-container">
-                  <h3>
-                    Svitlana Otenko, <span>TechLeader, Developer</span>
-                  </h3>
-                  <p>
-                    Developed part: backend: deployment of a server for
-                    development, database, routes, controllers (authControllers,
-                    monthControllers, todayControllers, userControllers,
-                    waterControllers, waterRateControllers), services and
-                    middleware, implementation of sending emails, storing images
-                    in the cloud, help with complex issues, frontend: help with
-                    complex issues
-                  </p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/feed/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={SK} alt="Serhii Krysiuk" />
-                </div>
+        <ul className="members">
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/svitlana-otenko/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={SO} alt="Svitlana Otenko" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Svitlana Otenko, <span>TechLeader, Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> backend: deployment of a server for
+                  development, database, routes, controllers (authControllers,
+                  monthControllers, todayControllers, userControllers,
+                  waterControllers, waterRateControllers), services and
+                  middleware, implementation of sending emails, storing images
+                  in the cloud, help with complex issues, frontend: help with
+                  complex issues
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="www.linkedin.com/in/nadiia-pavliuchenko"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={NP} alt="Nadiia Pavliuchenko" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Nadiia Pavliuchenko, <span>TechLeader, Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> deployment of a server for development,
+                  routes, SettingsModal, implementation of dark and light themes
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/andrii-dovhaniuk-ua/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={AD} alt="Andrii Dovhaniuk" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Andrii Dovhaniuk, <span>SCRUM, Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> redux state, api thunks, addWaterModal,
+                  animation, loader, 404 page, deleteUserModal/Form,
+                  modalContainer, wrapper, recoveryModal/Form,
+                  resendCodeModal/Form, userLogoutModal, waterRatioPanel,
+                  notifications, services, help with complex issues.
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/andrii-koliadenko/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={AK} alt="Andrii Koliadenko" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Andrii Koliadenko, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> Calendar, TodayListModal
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href=" https://www.linkedin.com/in/ivan-imenynnyk/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={II} alt="Ivan Imenynnyk" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Ivan Imenynnyk, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> DailyNormaModal
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/irynatrysh/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={IT} alt="Iryna Trysh" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Iryna Trysh, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> HomePage
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/bogdan-solomich/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={BS} alt="Bohdan Solomych" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Bohdan Solomych, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> Header, UserLogo
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/dana-dobryshkina-ua"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={DD} alt="Dana Dobryshkina" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Dana Dobryshkina, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> WelcomePage, WhyDrinkWater, Main,
+                  WaterTrackerConsumption, partially DailyNorma
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/volodymyr-shevchenko-developer/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={VS} alt=" Volodymyr Shevchenko" />
+              </div>
+              <div className="desc-container">
+                <h3>
+                  Volodymyr Shevchenko, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> SignInPage, SignUpPage, SignIn/SignUp
+                  form, TodayWaterList, DeleteWaterIntakesModal
+                </p>
+              </div>
+            </a>
+          </li>
 
-                <div className="desc-container">
-                  <h3>
-                    Serhii Krysiuk, <span>Developer</span>
-                  </h3>
-                  <p>
-                    Developed part: partially updateWaterIntakeRecord,
-                    removeWaterIntakeRecord, sendPasswordRecoveryEmail and
-                    recoverPassword, frontend: UserLogoModal, styling DailyNorma
-                  </p>
-                </div>
-              </a>
-            </li>
-            <li className="member">
-              <a
-                href="https://www.linkedin.com/in/oleksiy-pokormyakho/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="img-container">
-                  <img src={OP} alt="Oleksii Pokormiakho" />
-                </div>{' '}
-                <div className="desc-container">
-                  <h3>
-                    Oleksii Pokormiakho, <span>Developer</span>
-                  </h3>
-                  <p>Developed part: Swagger endpoint documentation</p>
-                </div>
-              </a>
-            </li>
-          </ul>
-        </div>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/serhii-krysiuk-4330052ab/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={SK} alt="Serhii Krysiuk" />
+              </div>
+
+              <div className="desc-container">
+                <h3>
+                  Serhii Krysiuk, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> partially updateWaterIntakeRecord,
+                  removeWaterIntakeRecord, sendPasswordRecoveryEmail and
+                  recoverPassword, frontend: UserLogoModal, styling DailyNorma
+                </p>
+              </div>
+            </a>
+          </li>
+          <li className="member">
+            <a
+              href="https://www.linkedin.com/in/oleksiy-pokormyakho/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-container">
+                <img src={OP} alt="Oleksii Pokormiakho" />
+              </div>{' '}
+              <div className="desc-container">
+                <h3>
+                  Oleksii Pokormiakho, <span>Developer</span>
+                </h3>
+                <p>
+                  <b>Developed part:</b> Swagger endpoint documentation
+                </p>
+              </div>
+            </a>
+          </li>
+        </ul>
       </Container>
     </Wrapper>
   );
--- src/pages/TeamPage/TeamPage.styled.js
+++ src/pages/TeamPage/TeamPage.styled.js
@@ -1,99 +1,130 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-weight: 700;
-  font-size: 36px;
-  line-height: 32px;
-  color: ${({ theme }) => theme.color.blue};
   text-align: center;
-  margin: 32px auto;
+  margin: 24px auto;
 `;
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 24px;
-
-  span {
-    color: ${({ theme }) => theme.color.skyBlue};
-  }
-  a {
+  .members {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     flex-wrap: wrap;
-    align-items: center;
-    justify-content: space-between;
-    gap: 8px;
-    text-decoration: none;
-    color: ${({ theme }) => theme.color.blue};
-  }
-  img {
-    width: 128px;
-    height: 128px;
-  }
-
-  ul {
-    display: flex;
-    flex-direction: column;
-
-    gap: 8px;
-  }
-
-  p {
-    text-align: start;
-  }
-
-  .front-end,
-  .back-end {
-    width: 100%;
-  }
-
-  .front-end-title {
-    margin-bottom: 12px;
-    text-align: start;
-  }
-
-  .membersFront {
+    justify-content: center;
     align-items: flex-start;
-  }
-
-  .back-end-title {
-    margin-bottom: 12px;
-    text-align: end;
-  }
-
-  .membersBack {
-    align-items: flex-end;
+    gap: 24px;
+    padding: 24px;
   }
 
   .member {
-    display: block;
-    width: 100%;
-    padding: 8px 12px;
+    width: 300px;
+  }
 
-    border-radius: 8px;
-    box-shadow: 4px 12px 12px 4px rgba(64, 123, 255, 0.3);
+  .member > a {
+    text-decoration: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 12px;
   }
 
   .img-container {
-    flex: 0 0 128px;
-    border: 1px solid ${({ theme }) => theme.color.blue};
+    width: 128px;
+    height: 128px;
     border-radius: 50%;
     overflow: hidden;
+  }
+
+  img {
+    width: 100%;
   }
 
   .desc-container {
     display: flex;
     flex-direction: column;
+    gap: 8px;
+    color: ${({ theme }) => theme.color.black};
   }
 
-  @media screen and (min-width: 768px) {
-    flex-direction: row;
-    justify-content: space-around;
-
-    a {
-      align-items: flex-start;
-    }
+  .bubble {
+    background: linear-gradient(
+      ${({ theme }) => theme.color.skyBlue},
+      ${({ theme }) => theme.color.blue}
+    );
+    border-radius: 50%;
+    box-shadow: 0 30px 15px rgba(0, 0, 0, 0.15);
+    position: absolute;
+  }
+  .bubble:before,
+  .bubble:after {
+    content: '';
+    background: linear-gradient(
+      ${({ theme }) => theme.color.skyBlue},
+      ${({ theme }) => theme.color.blue}
+    );
+    border-radius: 50%;
+    box-shadow: 0 30px 15px rgba(0, 0, 0, 0.15);
+    position: absolute;
+  }
+  .bubble:nth-child(1) {
+    top: 15vh;
+    left: 15vw;
+    height: 22vmin;
+    width: 22vmin;
+  }
+  .bubble:nth-child(1):before {
+    width: 13vmin;
+    height: 13vmin;
+    bottom: -25vh;
+    right: -10vmin;
+  }
+  .bubble:nth-child(2) {
+    top: 20vh;
+    left: 38vw;
+    height: 10vmin;
+    width: 10vmin;
+  }
+  .bubble:nth-child(2):before {
+    width: 5vmin;
+    height: 5vmin;
+    bottom: -10vh;
+    left: -8vmin;
+  }
+  .bubble:nth-child(3) {
+    top: 12vh;
+    right: 30vw;
+    height: 13vmin;
+    width: 13vmin;
+  }
+  .bubble:nth-child(3):before {
+    width: 3vmin;
+    height: 3vmin;
+    bottom: -15vh;
+    left: -18vmin;
+    z-index: 6;
+  }
+  .bubble:nth-child(4) {
+    top: 25vh;
+    right: 18vw;
+    height: 18vmin;
+    width: 18vmin;
+  }
+  .bubble:nth-child(4):before {
+    width: 7vmin;
+    height: 7vmin;
+    bottom: -10vmin;
+    left: -15vmin;
+  }
+  .bubble:nth-child(5) {
+    top: 60vh;
+    right: 18vw;
+    height: 28vmin;
+    width: 28vmin;
+  }
+  .bubble:nth-child(5):before {
+    width: 10vmin;
+    height: 10vmin;
+    bottom: 5vmin;
+    left: -25vmin;
   }
 `;
